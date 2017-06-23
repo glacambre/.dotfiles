b38d519e6e324b7cd2186715d82696e34512928d
glacambre <code@lacamb.re>
2017-06-24 00:00:00 +0200

    Fixed indentation of ps1.zsh and vi-mode-color-prompt.zsh

--- default/.config/zsh/ps1.zsh
+++ default/.config/zsh/ps1.zsh
@@ -1,7 +1,9 @@
 # We need this to dynamically add user to prompt
+
 setopt PROMPT_SUBST
 PS1='}$([ "$USER" != "me" ] && echo $USER)'
 parentprocess="$(ps -o comm= -p $PPID)"
+
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
     PS1="}%n@%M"
 else
@@ -9,7 +11,9 @@ else
         sshd|*/sshd) PS1="}%n@%M" ;;
     esac
 fi
+
 case "$parentprocess" in
     *sh) PS1="$PS1% ∞" ;;
 esac
+
 export PS1="$PS1%~{"
--- default/.config/zsh/vi-mode-color-prompt.zsh
+++ default/.config/zsh/vi-mode-color-prompt.zsh
@@ -1,20 +1,24 @@
 # Color PS1
 export KEYTIMEOUT=1
 export _PS1="$PS1"
+
 precmd() {
-  PS1="%{[38;05;4m%}$_PS1%{[0m%}"
+    PS1="%{[38;05;4m%}$_PS1%{[0m%}"
 }
+
 zle-keymap-select() {
-if [[ "$KEYMAP" = "vicmd" ]] ; then
-  PS1="%{[38;05;2m%}$_PS1%{[0m%}"
-else
-  PS1="%{[38;05;4m%}$_PS1%{[0m%}"
-fi
-() { return $__prompt_status }
-zle reset-prompt
-  }
-  zle-line-init() {
-  typeset -g __prompt_status="$?"
+    if [[ "$KEYMAP" = "vicmd" ]] ; then
+        PS1="%{[38;05;2m%}$_PS1%{[0m%}"
+    else
+        PS1="%{[38;05;4m%}$_PS1%{[0m%}"
+    fi
+    () { return $__prompt_status }
+    zle reset-prompt
 }
+
+zle-line-init() {
+    typeset -g __prompt_status="$?"
+}
+
 zle -N zle-keymap-select
 zle -N zle-line-init
