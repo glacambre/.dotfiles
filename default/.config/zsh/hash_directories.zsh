25336eac41eb5dd21b31a5c949a10109cfc97c00
glacambre <me@x250>
2017-10-19 12:49:55 +0200

    Add ip hashed directory.

--- default/.config/zsh/hash_directories.zsh
+++ default/.config/zsh/hash_directories.zsh
@@ -4,11 +4,11 @@ hash -d pnl=~/documents/ecole/6/pnl
 hash -d pc2r=~/documents/ecole/6/pc2r
 
 hash -d aaga=~/documents/ecole/7/aaga
-hash -d dar=~/documents/ecole/7/dar
-hash -d tas=~/documents/ecole/7/tas
-hash -d svp=~/documents/ecole/7/svp
-hash -d ppc=~/documents/ecole/7/ppc
 hash -d gral=~/documents/ecole/7/gral
+hash -d ip=~/documents/ecole/7/ip
+hash -d ppc=~/documents/ecole/7/ppc
+hash -d svp=~/documents/ecole/7/svp
+hash -d tas=~/documents/ecole/7/tas
 
 hash -d nvim=~/.config/nvim
 hash -d zsh=~/.config/zsh
