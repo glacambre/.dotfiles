f039e7831a00785565856e6a9262661b08051b18
glacambre <code@lacamb.re>
2018-01-04 05:50:47 +0100

    nvim.zsh: Remove unnecessary nvim_save_prompt_pos call

--- default/.config/zsh/nvim.zsh
+++ default/.config/zsh/nvim.zsh
@@ -12,7 +12,8 @@ chpwd_functions+=nvim_cd
 function nvim_save_prompt_pos () {
     nvimsavepromptpos
 }
-(nvim_save_prompt_pos &) >/dev/null
+# Apparently not needed
+# (nvim_save_prompt_pos &) >/dev/null
 if [[ ! -v precmd_functions ]]; then
     precmd_functions=()
 fi
