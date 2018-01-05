c99ee5847ff0adb739e7b26374a30eb67114abda
glacambre <code@lacamb.re>
2018-01-05 16:57:20 +0100

    vi-mode-color-prompt.zsh: Fix bug on multi-line zsh prompts

--- default/.config/zsh/vi-mode-color-prompt.zsh
+++ default/.config/zsh/vi-mode-color-prompt.zsh
@@ -4,17 +4,17 @@ export _PS1="$PS1"
 
 set_insert_colors() {
     if [[ $__prompt_status == 0 ]]; then
-        PS1="[44m[34m$_PS1[0m"
+        PS1="%{[34;44m%}$_PS1%{[0m%}"
     else
-        PS1="[41m[31m$_PS1[0m"
+        PS1="%{[31;41m%}$_PS1%{[0m%}"
     fi
-    print -n -- "\033[6 q"
+    # print -n -- "\033[6 q"
 }
 
 zle-keymap-select() {
     if [[ "$KEYMAP" = "vicmd" ]] ; then
         # Green, block cursor
-        PS1="[42m[32m$_PS1[0m"
+        PS1="%{[42;32m%}$_PS1%{[0m%}"
         print -n -- "\033[2 q"
     else
         set_insert_colors
@@ -22,13 +22,14 @@ zle-keymap-select() {
     zle reset-prompt
 }
 
-if [[ ! -v precmd_functions ]]; then
-    precmd_functions=()
-fi
 precmd_setup_colors() {
     typeset -g __prompt_status="$?"
     set_insert_colors
 }
+
+if [[ ! -v precmd_functions ]]; then
+    precmd_functions=()
+fi
 precmd_functions+=precmd_setup_colors
 
 zle -N zle-keymap-select
