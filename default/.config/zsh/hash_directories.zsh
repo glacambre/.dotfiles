9a314e1d958d5341592b8bbd59b1ac79af6281e5
glacambre <code@lacamb.re>
2018-02-14 19:02:06 +0100

    hash_directories.zsh: Remove old hashes

--- default/.config/zsh/hash_directories.zsh
+++ default/.config/zsh/hash_directories.zsh
@@ -1,17 +1,5 @@
-hash -d aps=~/documents/ecole/6/aps
-hash -d cpa=~/documents/ecole/6/cpa
-hash -d pnl=~/documents/ecole/6/pnl
-hash -d pc2r=~/documents/ecole/6/pc2r
-
-hash -d aaga=~/documents/ecole/7/aaga
-hash -d gral=~/documents/ecole/7/gral
-hash -d ip=~/documents/ecole/7/ip
-hash -d ppc=~/documents/ecole/7/ppc
-hash -d stage=~/documents/ecole/7/stage
-hash -d svp=~/documents/ecole/7/svp
-hash -d tas=~/documents/ecole/7/tas
-
 hash -d nvim=~/.config/nvim
 hash -d zsh=~/.config/zsh
+hash -d shelley=~/.config/nvim/bundle/repos/github.com/glacambre/shelley
 
 hash -d cv=~/documents/cv_motiv/src
