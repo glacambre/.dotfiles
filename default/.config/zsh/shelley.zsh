c0ee8e3164b68803df4192fd09d8455b40e30e0f
glacambre <code@lacamb.re>
2018-04-23 10:45:57 +0200

    shelley.zsh: Fix shelley url and typo in variable name

--- default/.config/zsh/shelley.zsh
+++ default/.config/zsh/shelley.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 ZSH_SHELLEY_DIR="$HOME/.config/nvim/bundle/repos/github.com/glacambre/shelley"
-ZSH_INTALL_FILE="/tmp/zsh_shelley_installation_error"
+ZSH_INSTALL_FILE="/tmp/zsh_shelley_installation_error"
 if [[ ! -d  "$ZSH_SHELLEY_DIR" && ! -e "$ZSH_INSTALL_FILE" ]]; then
     echo "Trying to install zsh-shelley."
     mkdir -p "$(dirname "$ZSH_SHELLEY_DIR")"
-    git clone "https://github.com/zsh-users/zsh-shelley" "$ZSH_SHELLEY_DIR" > "$ZSH_INTALL_FILE"
+    git clone "https://github.com/glacambre/zsh-shelley" "$ZSH_SHELLEY_DIR" > "$ZSH_INSTALL_FILE"
     if [ "$?" != "0" ]; then
         echo "Failed to download zsh-shelley."
     fi
