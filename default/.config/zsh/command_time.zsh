430f3183f6a70f52f5fdb343fd9400eb3967fbc7
glacambre <code@lacamb.re>
2018-07-22 14:31:53 +0200

    command_time.zsh: Notify-send about finished jobs when possible

--- default/.config/zsh/command_time.zsh
+++ default/.config/zsh/command_time.zsh
@@ -1,9 +1,20 @@
 
 function display_command_time() {
+    cmd="$(history | tail -n 1 | cut -d' ' -f2-)"
     if [[ "$LAST_COMMAND_TIME" != "" ]]; then
         timespan="$(($(date '+%s') - $LAST_COMMAND_TIME))"
         if [[ "$timespan" -gt 120 ]]; then
             echo "Job took $timespan seconds."
+            if which notify-send >/dev/null && which dunst >/dev/null; then
+                notify="timeout 3 notify-send 'Command finished in $timespan seconds' '$cmd'"
+                (
+                if ! $(eval "$notify") ; then
+                    dunst &
+                    sleep 1
+                    eval $notify
+                fi
+                ) 2>/dev/null >/dev/null &|
+            fi
         fi
     fi
 }
