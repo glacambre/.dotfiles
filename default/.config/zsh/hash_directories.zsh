8e6e6a9b0188f74f7524752d6df21d9f8ccde4e1
glacambre <code@lacamb.re>
2018-10-03 10:25:20 +0200

    Move ~/prog/*/* to ~/prog, update tridactyl dir

--- default/.config/zsh/hash_directories.zsh
+++ default/.config/zsh/hash_directories.zsh
@@ -2,6 +2,6 @@ hash -d nvim=~/.config/nvim
 hash -d zsh=~/.config/zsh
 hash -d shelley=~/.config/nvim/bundle/repos/github.com/glacambre/shelley
 hash -d usb=/var/run/usbmount
-hash -d tridactyl=~/prog/js/tridactyl
+hash -d tridactyl=~/prog/tridactyl
 
 hash -d cv=~/documents/cv_motiv/src
