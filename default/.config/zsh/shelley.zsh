0ced6c50d811cf28d69d7b967e16579e034a3f9f
glacambre <code@lacamb.re>
2020-04-21 06:51:24 +0200

    shelley.zsh: fix repo url and clone directory

--- default/.config/zsh/shelley.zsh
+++ default/.config/zsh/shelley.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
-ZSH_SHELLEY_DIR="$HOME/.config/nvim/bundle/repos/github.com/glacambre/shelley"
+ZSH_SHELLEY_DIR="$HOME/.config/nvim/pack/minpac/start/shelley"
 ZSH_INSTALL_FILE="/tmp/zsh_shelley_installation_error"
 if [[ ! -d  "$ZSH_SHELLEY_DIR" && ! -e "$ZSH_INSTALL_FILE" ]]; then
     echo "Trying to install zsh-shelley."
     mkdir -p "$(dirname "$ZSH_SHELLEY_DIR")"
-    git clone "https://github.com/glacambre/zsh-shelley" "$ZSH_SHELLEY_DIR" > "$ZSH_INSTALL_FILE"
+    git clone "https://github.com/glacambre/shelley" "$ZSH_SHELLEY_DIR" > "$ZSH_INSTALL_FILE"
     if [ "$?" != "0" ]; then
         echo "Failed to download zsh-shelley."
     fi
