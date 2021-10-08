1709733a6ae4ac65a85a7a732011cb9a876d639a
glacambre <code@lacamb.re>
2021-10-08 08:02:55 +0200

    zhs: fix ileum install script

--- default/.config/zsh/zsh_ileum.zsh
+++ default/.config/zsh/zsh_ileum.zsh
@@ -5,7 +5,7 @@ ZSH_INTALL_FILE="/tmp/zsh_ileum_installation_error"
 if [[ ! -d  "$ZSH_ILEUM_DIR" && ! -e "$ZSH_INSTALL_FILE" ]]; then
     echo "Trying to install zsh-ileum."
     mkdir -p "$(dirname "$ZSH_ILEUM_DIR")"
-    git clone "https://github.com/glacambre/zsh-ileum" "$ZSH_ILEUM_DIR" > "$ZSH_INTALL_FILE"
+    git clone "https://github.com/glacambre/ileum.git" "$ZSH_ILEUM_DIR" > "$ZSH_INTALL_FILE"
     if [ "$?" != "0" ]; then
         echo "Failed to download zsh-ileum."
     fi
